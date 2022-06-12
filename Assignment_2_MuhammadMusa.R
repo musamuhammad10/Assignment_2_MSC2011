98107d22d44297a520818750bc73889969cc937e
musamuhammad10 <muhammadmusa1004@gmail.com>
2022-06-11 21:39:59 -0400

    Final - added relevant comments, adding appropriate variable names, edited overall structure and ensured the program runs as expected.

--- Assignment_2_MuhammadMusa.R
+++ Assignment_2_MuhammadMusa.R
@@ -1,29 +1,36 @@
-# Prompt the user to enter a three digit positive number
+# Muhammad Musa
+# MSC2011 Assignment 2
+# June 10, 2022
 
+# Prompt the user to enter a three digit positive number.
 usernum <- readline("Please enter a three digit positive number: ")
-# Check if the user input is numeric. If not, print an error message and quit.
 
+# Convert user input from character type to numeric.
 usernum <- as.numeric(usernum)
 
+# Check if the user input is numeric and a positive 3 digit number. If not, print an error message and quit.
+# If input is valid, continue to check if the number is narcissistic.
 if (is.na(usernum) | usernum < 100 ) {
-  print("Invalid entry, program is terminating. Please try again")
+  print("INVALID ENTRY, program is terminating. Please try again!")
 } else {
   
+  # Checking if the user's input is a narcissistic number and printing the appropriate message
+  # Converting user input into a character type again in order to use the strsplit function to split the elements of the character vector.
+  # This function would split the user's input into individual digits (elements). This result is converted back to numeric type and stored as a variable, 'digit'.
   digit <- as.numeric(strsplit(as.character(usernum),"")[[1]])
-  x <- digit^3
-  y <- sum(x)
   
-  if (usernum == y) {
-    print(paste(usernum, "is a nacissistic number"))
+  # Arithmetic to first cube each element of the input and then summing them up.
+  digit_cubed <- digit^3
+  sum_digit_cubed <- sum(digit_cubed)
+  
+  # Comparing user's input with the calculated sum to determine if user's input is a narcissistic number.
+  if (usernum == sum_digit_cubed) {
+    print(paste(usernum, "is a narcissistic number!"))
   } else {
-    print(paste(usernum, "is not nacissistic number"))
+    print(paste(usernum, "is not narcissistic number!"))
     
   }
 }
 
-# Check if the number is narcissistic. A narcissistic number, or an Armstrong number, is a number that is equal to the 
-# sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
 
-# Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” 
-# or any additional fun text you may want to add.
 
